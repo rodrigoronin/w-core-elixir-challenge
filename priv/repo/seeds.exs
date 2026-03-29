36676f1c1f5356c49995cc2de53fb4f5ce40a028
Rodrigo 'Kraby' Lira <liramailbox@gmail.com>
2026-03-29 01:32:24 -0300

    feat: add database seeds for telemetry nodes to support local testing and demo

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,22 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     ElixirTelemetryEngine.Repo.insert!(%ElixirTelemetryEngine.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias ElixirTelemetryEngine.Repo
+alias ElixirTelemetryEngine.Telemetry
+alias ElixirTelemetryEngine.Telemetry.Node
+
+nodes = [
+  %{machine_identifier: "sensor_1", location: "line_a"},
+  %{machine_identifier: "sensor_2", location: "line_a"},
+  %{machine_identifier: "sensor_3", location: "line_b"},
+  %{machine_identifier: "sensor_4", location: "line_c"},
+  %{machine_identifier: "sensor_5", location: "line_c"}
+]
+
+Enum.each(nodes, fn attrs ->
+  case Repo.get_by(Node, machine_identifier: attrs.machine_identifier) do
+    nil ->
+      {:ok, _} = Telemetry.create_node(attrs)
+      IO.puts("Created #{attrs.machine_identifier}")
+
+    _node ->
+      IO.puts("Already exists: #{attrs.machine_identifier}")
+  end
+end)
